08f82dd2d3b4c2bf9289248ca316982216018470
Ruben Zantingh <ruben@e-tales.nl>
2024-11-10 12:51:08 +0100

    Create release on tag push / CF upload experimenting

---
+++ cf-upload.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+RELEASE_NAME="$1"
+RELEASE_MESSAGE="$2"
+GAME_VERSION=110005
+ADDON_LANG="RU"
+
+CF_METADATA=$(cat <<-EOF
+{
+    "displayName": "$RELEASE_NAME",
+    "releaseType": "release",
+    "changelog": $RELEASE_MESSAGE,
+    "changelogType": "markdown",
+    "gameVersions": [$GAME_VERSION]
+}
+EOF
+)
+
+response=$(curl -sS \
+    -o response.txt \
+    -w "%{http_code}" \
+    -H "X-API-TOKEN: $CF_API_TOKEN" \
+    -F "metadata=$CF_METADATA" \
+    -F "file=@releases/$RELEASE_NAME/MultiLanguage_$ADDON_LANG.zip" \
+    "https://wow.curseforge.com/api/projects/965777/upload-file")
+
+http_status=$(echo "$response" | tail -n1)
+
+if [ "$http_status" -eq 200 ]; then
+  echo "CurseForge upload successful"
+else
+  echo "CurseForge upload failed, HTTP-code: $http_status"
+  cat response.txt
+  exit 1
+fi
